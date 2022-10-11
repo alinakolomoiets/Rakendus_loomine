11c5132b3271ad859892aa68e423748af9cee25d
opilane <opilane@HP-CZC6297B8C>
2022-10-11 14:12:59 +0300

    Add project files.

---
+++ Rakendus_loomine/Pildid.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rakendus_loomine
+{
+	public partial class Pildid : Form
+	{
+            Button selge;
+            Button sulge;
+            Button näidata;
+            PictureBox pilt;
+            TableLayoutPanel tableLayoutPanel1;
+            CheckBox box;
+            OpenFileDialog openFileDialog;
+            public Pildid()
+            {
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Pildid";
+
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.Controls.Add(this.tableLayoutPanel1);
+
+            pilt = new PictureBox()
+            {
+                BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                Location = new System.Drawing.Point(2, 2),
+                Size = new System.Drawing.Size(100, 100),
+                TabIndex = 0,
+                TabStop = false
+            };
+            tableLayoutPanel1.Controls.Add(pilt, 0, 0);
+            tableLayoutPanel1.SetCellPosition(pilt, new TableLayoutPanelCellPosition(0, 0));
+            tableLayoutPanel1.SetColumnSpan(pilt, 2);
+
+
+            box = new CheckBox()
+            {
+                Text = "box",
+                AutoSize = true,
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+
+            };
+            tableLayoutPanel1.Controls.Add(box, 0, 1);
+            box.CheckedChanged += Stretch_CheckedChanged;
+
+
+            sulge = new Button()
+            {
+                Text = "Kinni",
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            };
+            selge = new Button()
+            {
+                Text = "Kustuta",
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+            };
+            näidata = new Button()
+            {
+                Text = "Näita",
+                BackColor = Color.White,
+                ForeColor = Color.Black,
+
+            };
+            näidata.Click += Tegevus;
+            selge.Click += Tegevus;
+            sulge.Click += Tegevus;
+            Button[] buttons = { selge, näidata, sulge };
+            FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel()
+            {
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                FlowDirection = FlowDirection.RightToLeft,
+                AutoSize = true,
+                WrapContents = false,
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            flowLayoutPanel.Controls.AddRange(buttons);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel, 1, 1);
+            this.Controls.Add(tableLayoutPanel1);
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All files (*.*)|*.*";
+
+        }
+
+        private void Stretch_CheckedChanged(object sender, EventArgs e)
+        {
+            if (box.Checked)
+                pilt.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pilt.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void Tegevus(object sender, EventArgs e)
+        {
+            Button nupp_sender = (Button)sender;
+            if (nupp_sender.Text == "Näita")
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pilt.Load(openFileDialog.FileName);
+                }
+            }
+            else if (nupp_sender.Text == "Kustuta")
+            {
+                pilt.Image = null;
+            }
+            else if (nupp_sender.Text == "Kinni")
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Rakendus_loomine/Sobitamise_mäng.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rakendus_loomine
+{
+	public partial class Sobitamise_mäng : Form
+	{
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+                "!", "!", "N", "N", ",", ",", "k", "k",
+                 "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        public Timer timer = new Timer { Interval = 750 };
+
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        Label lbl;
+        TableLayoutPanel tableLayoutPanel1;
+        public Sobitamise_mäng()
+		{
+
+            this.Size = new System.Drawing.Size(550, 550);
+            this.Text = "Sobitamise Mäng";
+            this.MaximizeBox = false;
+
+
+            tableLayoutPanel1 = new TableLayoutPanel()
+            {
+                ColumnCount = 4,
+                Location = new System.Drawing.Point(3, 4),
+                Name = "tableLayoutPanel1",
+                RowCount = 4,
+                Size = new System.Drawing.Size(527, 506),
+                TabIndex = 0,
+                CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset,
+                BackColor = System.Drawing.Color.CornflowerBlue,
+            };
+
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+
+            this.Controls.Add(this.tableLayoutPanel1);
+
+
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "c",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 0, 0);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "w",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 0, 1);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "!",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 0, 2);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "N",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 0, 3);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = ",",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 1, 0);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "z",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 1, 1);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "k",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 1, 2);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "b",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 1, 3);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "v",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 2, 0);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "!",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 2, 1);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "w",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 2, 2);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = ",",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 2, 3);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "c",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 3, 0);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "b",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 3, 1);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "z",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 3, 2);
+            lbl = new Label()
+            {
+                BackColor = System.Drawing.Color.CornflowerBlue,
+                AutoSize = false,
+                Dock = System.Windows.Forms.DockStyle.Fill,
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter,
+                Font = new System.Drawing.Font("Webdings", 48, System.Drawing.FontStyle.Bold),
+                Text = "k",
+            };
+            tableLayoutPanel1.Controls.Add(lbl, 3, 3);
+            tableLayoutPanel1.Controls.Add(lbl, 3, 3);
+            AssignIconsToSquares();
+        }
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+            }
+        }
+
+    }
+}
